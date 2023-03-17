2db8105f750fdf7bee37a2093f5513773d3dff26
student <elizarova.katerina2018@yandex.ru>
2023-03-17 15:47:53 +0300

    z4

---
+++ z4.py
@@ -0,0 +1,11 @@
+color1 = input("Введите первый цвет: ")
+color2 = input("Введите второй цвет: ")
+
+if color1 == "red" and color2 == "yellow" or color1 == "yellow" and color2 == "red":
+    print("Оранжевый")
+elif color1 == "red" and color2 == "blue" or color1 == "blue" and color2 == "red":
+    print("Фиолетовый")
+elif color1 == "blue" and color2 == "yellow" or color1 == "yellow" and color2 == "blue":
+    print("Зеленый")
+else:
+    print("Чел ты...")
